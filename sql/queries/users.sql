f5feb6e71b6b4684c5ffdf7301dc9381fdfbe30d
stkisengese <skisengese@outlook.com>
2025-05-02 10:52:32 +0300

    feat: create a db query

---
+++ sql/queries/users.sql
@@ -0,0 +1,11 @@
+-- name: CreateUser :one
+INSERT INTO users (id, created_at, updated_at, name)
+VALUES ($1, $2, $3, $4)
+RETURNING *;
+
+-- name: GetUser :one
+SELECT * FROM users 
+WHERE name = $1 LIMIT 1;
+
+-- name: UserExists :one
+SELECT EXISTS(SELECT 1 FROM users WHERE name = $1);
